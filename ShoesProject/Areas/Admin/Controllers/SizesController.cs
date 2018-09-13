a0f970b6f432772a3bde9b7d9f83c91378dafe0f
LAB08-PC21\Lab06 <trantung070809@gmail.com>
2018-09-13 20:57:49 +0700

    Tung commit Size

---
+++ ShoesProject/Areas/Admin/Controllers/SizesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TungTT25.Model;
+
+namespace ShoesProject.Areas.Admin.Controllers
+{
+    public class SizesController : Controller
+    {
+        ShoesModel db = new ShoesModel();
+        // GET: Admin/Sizes
+        public ActionResult Index()
+        {
+            return View(db.Sizes.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Size s)
+        {
+            s.SizeStatus = s.SizeStatus ?? false;
+
+            db.Sizes.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var size = db.Sizes.Find(id);
+
+            return View(size);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Size s)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(s).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(s);
+        }
+    }
+}
