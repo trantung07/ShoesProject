291d9d756a695e919e2f89fd1cddd15c893eea1b
phamhung93 <phamsyhung93@gmail.com>
2018-09-13 16:55:24 +0700

    Login Register fontend

--- ShoesProject/Controllers/LoginController.cs
+++ ShoesProject/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hung.Model;
+using ShoesProject.Areas.Admin.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,50 @@ namespace ShoesProject.Controllers
 {
     public class LoginController : Controller
     {
+        HungModel db = new HungModel();
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Register(User u)
+        {
+
+            var user = db.Users.SingleOrDefault(x => x.Email.Equals(u.Email));
+            if (user == null)
+            {
+                u.Password = Utility.getHashedMD5(u.Password);
+                db.Users.Add(u);
+                db.SaveChanges();
+                TempData["smg"] = "Đăng ký Thành Công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                TempData["smg"] = "Email đã đc đăng ký ";
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult LoginUser(string Email, string Password)
+        {
+
+            var pass = Utility.getHashedMD5(Password);
+            var user = db.Users.SingleOrDefault(x => x.Email.Equals(Email) && x.Password.StartsWith(pass) && x.Password.EndsWith(pass));
+            if (user != null)
+            {
+                Session["user"] = user.UserName;
+                return RedirectToAction("Index", "MyAccount");
+            }
+            else
+            {
+                TempData["msg"] = "Đăng nhập thất bại";
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
