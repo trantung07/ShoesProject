2ed04bc98959d8e8b6078ae36deb31d20874dca5
phamhung93 <phamsyhung93@gmail.com>
2018-09-16 00:11:34 +0700

    gio hang

--- ShoesProject/Controllers/LoginController.cs
+++ ShoesProject/Controllers/LoginController.cs
@@ -19,23 +19,28 @@ namespace ShoesProject.Controllers
         [HttpPost]
         public ActionResult Register(User u)
         {
-
-            var user = db.Users.SingleOrDefault(x => x.Email.Equals(u.Email));
-            if (user == null)
+            try
             {
-                u.Password = Utility.getHashedMD5(u.Password);
-                db.Users.Add(u);
-                db.SaveChanges();
-                TempData["smg"] = "Đăng ký Thành Công";
-                return RedirectToAction("Index");
+                var user = db.Users.SingleOrDefault(x => x.Email.Equals(u.Email));
+                if (user == null)
+                {
+                    u.Password = Utility.getHashedMD5(u.Password);
+                    db.Users.Add(u);
+                    db.SaveChanges();
+                    TempData["smg"] = "Đăng ký Thành Công";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["smg"] = "Email đã đc đăng ký ";
+                    return RedirectToAction("Index");
+                }
             }
-            else
+            catch (Exception)
             {
 
-                TempData["smg"] = "Email đã đc đăng ký ";
-                return RedirectToAction("Index");
+                throw;
             }
-
         }
         [HttpPost]
         public ActionResult LoginUser(string Email, string Password)
