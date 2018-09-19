e5a3d3e13dfa934faaf49dd6884119fac722c11a
mb <minhcf2013@gmail.com>
2018-09-19 10:24:34 +0800

    add temporary permisson controller

---
+++ ShoesProject/Areas/Admin/Controllers/SetPermissonController.cs
@@ -0,0 +1,35 @@
+﻿using ShoesProjectModels.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoesProject.Areas.Admin.Controllers
+{
+    public class SetPermissonController : Controller
+    {
+        Shoes db = new Shoes();
+        // GET: Admin/SetPermisson
+        public ActionResult Index(int id)
+        {
+            var listBusinesses = db.Businesses.AsEnumerable();
+            List<SelectListItem> items = new List<SelectListItem>();
+            foreach (var item in listBusinesses)
+            {
+                items.Add(new SelectListItem() { Text = item.BusinessName, Value = item.BusinessId });
+            }
+            ViewBag.items = items;
+
+            var listGrantedPermissons = from a in db.Admins
+                                        from p in a.Permissons
+                                        where a.AdminId == id
+                                        select new SelectListItem() { Text = p.PermissonDescription, Value = p.PermissonId.ToString() };
+            ViewBag.listGrantedPermissons = listGrantedPermissons;
+            Session["userOnSet"] = id;
+            var userOnSet = db.Admins.Find(id);
+            ViewBag.userOnSet = userOnSet;
+            return View();
+        }
+    }
+}
---
+++ ShoesProject/Areas/Admin/Models/PermissonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoesProject.Areas.Admin.Models
+{
+    public class PermissonViewModel
+    {
+        public int PermissonId { get; set; }
+        public string PermissonName { get; set; }
+        public string Description { get; set; }
+        public bool isGranted { get; set; }
+    }
+}
