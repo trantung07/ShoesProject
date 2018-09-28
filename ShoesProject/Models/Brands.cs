ac85ec71c3bb1e252da618b41ee5e4c510611cdc
AnhLVV <anhlvv@ows.com.vn>
2018-09-28 23:20:59 +0700

    ListProduct release - AnhLVV

---
+++ ShoesProject/Models/Brands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoesProject.Models
+{
+    public class Brands
+    {
+        public int id { get; set; }
+
+        public string image { get; set; }
+
+        public string name { get; set; }
+
+        public int product { get; set; }
+
+        public Brands()
+        {
+
+        }
+
+        public Brands(int id, string image, string name, int product)
+        {
+            this.id = id;
+            this.image = image;
+            this.name = name;
+            this.product = product;
+        }
+    }
+}
